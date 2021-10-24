945df560ac887906f052da80f555d3d662b30c75
Mahdi Suleiman <90200717+Mahdi-Suleiman@users.noreply.github.com>
2021-10-24 11:04:35 +0300

    styles and stuff

---
+++ media-query/projectTwo-QuizGame--ghassan/quiz.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('.quiz-btn');
+const quizRules = document.querySelector('.quiz-rules');
+const quiz = document.querySelector('.quiz');
+
+btn.addEventListener('click', ()=>{
+  location.href="quiz.html";
+});
